e6e254b46f64ed6d1583432760d95607f544f698
YaangZhang <yangzhang_1@126.com>
2019-06-21 17:29:59 +0800

    大量数据导入

--- spring-boot-bigdata/src/main/java/com/sto/MysqlTestController.java
+++ spring-boot-bigdata/src/main/java/com/sto/MysqlTestController.java
@@ -14,12 +14,16 @@ package com.sto;
  */
 
 import com.sto.modle.User;
+import com.sto.modle.service.InsterService;
+import com.sto.modle.service.MultiInsert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -37,96 +41,39 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 @RestController
 public class MysqlTestController {
+
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private MultiInsert multiInsert;
+    @Autowired
+    private InsterService insterService;
 
     @GetMapping(value = "mulTest")
     public String mulTest(Integer size, @RequestParam(defaultValue = "false") boolean sharding) {
-        BlockingQueue<User> userQueue = initUserQueue(size);
-        return size + " 条记录插入，耗时" + processQueue(userQueue, sharding);
+        BlockingQueue<User> userQueue = multiInsert.initUserQueue(size);
+        System.out.println("开始时间 ： "+new Date());
+        long mulTime = multiInsert.processQueue(userQueue, sharding);
+        return size + " 条记录插入，耗时 " + mulTime+" ms ,   速度 ："+(size / mulTime * 1000)+" 条/s";
     }
 
-    private long processQueue(BlockingQueue<User> userQueue, boolean sharding) {
-        int mulSize = 200;
-        long beginTime = System.currentTimeMillis();
+
+    @GetMapping(value = "mulTest")
+    public String insertTest(@PathVariable int size,@PathVariable int batchSize, @RequestParam(defaultValue = "false") boolean sharding) {
+        System.out.println("开始时间 ： "+ new Date());
+        long start = System.currentTimeMillis();
         try {
             if (sharding) {
-                while (!userQueue.isEmpty()) {
-                    User user = userQueue.take();
-                    StringBuffer singleInserSql = new StringBuffer();
-                    singleInserSql.append("INSERT INTO `user` (`id`, `user_name`, `pass_word`, `age`, `city`, `email`, `nick_name`, `reg_time`) VALUES (?,?,?,?,?,?,?,?)");
-                    jdbcTemplate.update(singleInserSql.toString(), new Object[]{
-                            user.getId(), user.getUserName(), user.getPassWord(), user.getAge(), user.getCity(), user.getEmail(), user.getNickName(), user.getRegTime() });
-                }
-            } else {
-                String insertSqlStr = genMulFixSqlStr(mulSize);
-                List<User> userList = new ArrayList<>();
-                while (!userQueue.isEmpty()) {
-                    User user = userQueue.take();
-                    userList.add(user);
-                    if (userList.size() == mulSize) {
-                        Object[] params = getMulParams(userList, userList.size());
-                        jdbcTemplate.update(insertSqlStr, params);
-                        userList.clear();
-                    }
-                }
-                //插入剩余的数据
-                if (!userList.isEmpty()) {
-                    String restInsertSql = genMulFixSqlStr(userList.size());
-                    Object[] params = getMulParams(userList, userList.size());
-                    jdbcTemplate.update(restInsertSql, params);
-                    userList.clear();
-                }
-            }
-        } catch (InterruptedException e) {
+                insterService.testInsert(size);
+            }else  insterService.testBatchInsert(size, batchSize);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         long endTime = System.currentTimeMillis();
-        return endTime - beginTime;
+        long time = endTime - start;
+        return size + " 条记录插入，耗时 " + time+" ms ,   速度 ："+(size / time * 1000)+" 条/s";
     }
 
-    private Object[] getMulParams(List<User> userList, int mulSize) {
-        Object[] params = new Object[mulSize * 8];
-        for (int i = 0; i < mulSize; i++) {
-            User userInfo = userList.get(i);
-            params[8 * i] = userInfo.getId();
-            params[8 * i + 1] = userInfo.getUserName();
-            params[8 * i + 2] = userInfo.getPassWord();
-            params[8 * i + 3] = userInfo.getAge();
-            params[8 * i + 4] = userInfo.getCity();
-            params[8 * i + 5] = userInfo.getEmail();
-            params[8 * i + 6] = userInfo.getNickName();
-            params[8 * i + 7] = userInfo.getRegTime();
-        }
-        return params;
-    }
-
-    private String genMulFixSqlStr(int mulSize) {
-        StringBuffer mulInserSql = new StringBuffer();
-        mulInserSql.append("INSERT INTO `user` (`id`, `user_name`, `pass_word`, `age`, `city`, `email`, `nick_name`, `reg_time`) VALUES (?,?,?,?,?,?,?,?)");
-        for (int i = 1; i < mulSize; i++) {
-            mulInserSql.append(",(?,?,?,?,?,?,?,?)");
-        }
-        return mulInserSql.toString();
-    }
-
-
-    private BlockingQueue<User> initUserQueue(Integer size) {
-        BlockingQueue<User> userQueue = new LinkedBlockingQueue<>();
-        for (int i = 1; i <= size; i++) {
-            User user = new User();
-            user.setCity("shanghai" + i);
-            user.setRegTime(new Date());
-            user.setAge(i * 2 + 10);
-            user.setEmail(i*100+"12334@qq.com");
-            user.setNickName("nickName"+i);
-            user.setId(i*1000);
-            user.setUserName("ping" + i);
-            user.setPassWord("15700" + i);
-            userQueue.add(user);
-        }
-        return userQueue;
-    }
 
 
 }
